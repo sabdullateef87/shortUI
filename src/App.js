3c8daae4673976b2b1092d32a5ed318214ea4cd4
Abdullateef Suleiman <abdullateef@macs-MacBook-Pro.local>
2021-10-14 16:32:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = React.useState("");
+  const [shortUrl, setShortUrl] = React.useState("");
+  const [copied, setCopied] = React.useState(false);
+  const handleSubmit = async (e) => {
+    const response = await axios.post("https://str9.herokuapp.com/url", {
+      url,
+    });
+    setShortUrl(response.data.short_url);
+    console.log(shortUrl);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 style={{ marginBottom: "40px" }}>url shortner</h1>
+        <input
+          style={{ padding: "5px 10px", width: "100%" }}
+          type="text"
+          placeholder="Paste Url"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <div className="return_link">
+        {shortUrl && (
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <a href={shortUrl} target="_blank" style={{ marginRight: "40px" }}>
+              {" "}
+              {shortUrl}
+            </a>
+            <CopyToClipboard text={shortUrl} onCopy={() => setCopied(true)}>
+              <button>Copy</button>
+            </CopyToClipboard>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
